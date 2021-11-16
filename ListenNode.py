d7c4494a44bf65bb75ee31e37047bb0a992f548e
jackbigej <jbigej@nd.edu>
2021-11-16 00:47:38 -0500

    working broadcast

---
+++ ListenNode.py
@@ -0,0 +1,30 @@
+#!/usr/bin/bash python3
+
+import sys
+import json
+from Node import Node
+
+def main():
+    if len(sys.argv) != 4:
+        return -1
+
+    try:
+        host = str(sys.argv[1])
+    except:
+        return -1
+
+    try:
+        port = int(sys.argv[2])
+    except:
+        return -1
+
+    try:
+        self_port = int(sys.argv[3])
+    except:
+        return -1
+        
+    node = Node(host, port, self_port)
+    node.listen()
+
+if __name__ == '__main__':
+	main()
---
+++ StartNode.py
@@ -0,0 +1,29 @@
+#!/usr/bin/bash python3
+
+import sys
+import json
+from Node import Node
+
+def main():
+    if len(sys.argv) != 4:
+        return -1
+
+    try:
+        host = str(sys.argv[1])
+    except:
+        return -1
+
+    try:
+        port = int(sys.argv[2])
+    except:
+        return -1
+
+    try:
+        self_port = int(sys.argv[3])
+    except:
+        return -1
+        
+    node = Node(host, port, self_port)
+
+if __name__ == '__main__':
+	main()
