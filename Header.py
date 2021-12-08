f96f4780ad031dfacc608c25a78ab830b5f66a76
Nicholas Locascio <nicklocascio@rainmaker.local>
2021-12-08 11:07:28 -0500

    quick comment change

--- Header.py
+++ Header.py
@@ -1,6 +1,6 @@
 class Header:
     def __init__(self, prev_hash):
         self.prev_hash = prev_hash
-        self.hash = None # probably comes from some function in Block
+        self.hash = None # set in mine function
         self.timestamp = None # initialize in mine function
         self.nonce = None # initialize in mine function
